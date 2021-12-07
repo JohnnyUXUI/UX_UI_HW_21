e8f5a48fa36af4082fd04e42e7bd868551e64ea4
JohnnyUXUI <johnny.jakubowicz@mail.utoronto.ca>
2021-12-06 19:56:17 -0500

    add tracking

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
+const bgColour = document.querySelector('.bg-colour')
+
+let load = 0
+
+let int = setInterval(blurring, 10)
+
+function blurring() {
+  load++
+
+  if (load > 99) {
+    clearInterval(int)
+  }
+
+  loadText.innerText = `${load}%`
+  loadText.style.opacity = scale(load, 0, 100, 1, 0)
+  bgColour.style.opacity = scale(load, 0, 100, 1, 0)
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
